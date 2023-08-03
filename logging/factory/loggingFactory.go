36fe23fbcd076ff4aa300e8433994428dcea0779
Chris Pearson <chris@pearsonnet.co.uk>
2023-08-03 11:25:39 +0100

    Fixed import cycling

--- logging/factory/loggingFactory.go
+++ logging/factory/loggingFactory.go
@@ -2,18 +2,36 @@ package factory
 
 import (
 	"fmt"
-	"github.com/hotels-baby/go-adaptors/logging"
-
 	"github.com/hotels-baby/go-adaptors/logging/google"
 	"github.com/hotels-baby/go-adaptors/logging/zap"
 )
 
+// Logger types.
+type LoggerType string
+
+const (
+	LoggerTypeZap    LoggerType = "Zap"    // Represents a Zap Logger type
+	LoggerTypeGoogle LoggerType = "Google" // Represents a Google Logger type
+)
+
+// Config struct
+type Config struct {
+	LogFilePath     string // This is required
+	GoogleProjectID string // This is optional
+}
+
+// Logger interface
+type Logger interface {
+	Error(message string, err error)
+	Info(message string, fields ...interface{})
+}
+
 // NewLogger creates a new logger based on the provided type.
-func NewLogger(t logging.LoggerType, config logging.Config) (logging.Logger, error) {
+func NewLogger(t LoggerType, config Config) (Logger, error) {
 	switch t {
-	case logging.LoggerTypeZap:
+	case LoggerTypeZap:
 		return zap.NewZapLoggerAdapter(config.LogFilePath)
-	case logging.LoggerTypeGoogle:
+	case LoggerTypeGoogle:
 		if config.GoogleProjectID == "" {
 			return nil, fmt.Errorf("google logger requires a Google Project ID")
 		}
